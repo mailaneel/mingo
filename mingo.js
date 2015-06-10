7887b5cbcfa8ddb95f6977f6b577b2eac04711d1
mailaneel <mailaneel@gmail.com>
2015-06-10 12:12:40 +1000

    removed streams from mingo

--- mingo.js
+++ mingo.js
@@ -195,51 +195,6 @@
     }
   };
 
-  if (nodeEnabled) {
-
-    var Transform = require('stream').Transform;
-    var util = require('util');
-
-    Mingo.Query.prototype.stream = function (options) {
-      return new Mingo.Stream(this, options);
-    };
-
-    /**
-     * Create a Transform class
-     * @param query
-     * @param options
-     * @returns {Mingo.Stream}
-     * @constructor
-     */
-    Mingo.Stream = function (query, options) {
-
-      if (!(this instanceof Mingo.Stream))
-        return new Mingo.Stream(query, options);
-
-      options = options || {};
-      _.extend(options, {objectMode: true});
-      Transform.call(this, options);
-      // query for this stream
-      this._query = query;
-    };
-    // extend Transform
-    util.inherits(Mingo.Stream, Transform);
-
-    Mingo.Stream.prototype._transform = function (chunk, encoding, done) {
-      if (_.isObject(chunk) && this._query.test(chunk)) {
-        if (_.isEmpty(this._query._projection)) {
-          this.push(chunk);
-        } else {
-          var cursor = new Mingo.Cursor([chunk], this._query);
-          if (cursor.hasNext()) {
-            this.push(cursor.next());
-          }
-        }
-      }
-      done();
-    };
-  }
-
   /**
    * Cursor to iterate and perform filtering on matched objects
    * @param collection
